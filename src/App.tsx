5d04f1fbc498cb052635d9b052fd56b897407e41
Aloxan12 <kvakvaaaak@gmail.com>
2024-10-16 08:54:08 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import './App.css'
-import { useState} from 'react'
+import {useState} from 'react'
 import {AppCheckbox} from 'todoList/components'
 import TodoList from 'todoList/TodoList'
 import {Editor} from "./components/Editor/Editor";
+import {FabricJsEditor} from "./components/FabricjsEditor/FabricjsEditor";
 
 function App() {
     const [value, setValue] = useState(false)
-  return (
-    <div>
-      <div>main</div>
-        <Editor />
-        {/*<Suspense fallback='Loader'>*/}
-        <AppCheckbox text={'check'}
-                     value={value}
-                     onChange={setValue}
-                     id={1} />
-        <TodoList />
-        {/*</Suspense>*/}
-    </div>
-  )
+    return (
+        <div>
+            <div>main</div>
+            <FabricJsEditor/>
+            <Editor/>
+            {/*<Suspense fallback='Loader'>*/}
+            <AppCheckbox text={'check'}
+                         value={value}
+                         onChange={setValue}
+                         id={1}/>
+            <TodoList/>
+            {/*</Suspense>*/}
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/FabricjsEditor/FabricjsEditor.tsx
@@ -0,0 +1,8 @@
+
+export const FabricJsEditor = () => {
+    return (
+        <div>
+            FabricJsEditor
+        </div>
+    );
+}
