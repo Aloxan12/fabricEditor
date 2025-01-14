5804f14253e0d37b9fa807c90d76fae208f659a6
Aloxan12 <kvakvaaaak@gmail.com>
2025-01-14 13:32:58 +0300

    init

--- src/components/SvgAttrEditor/SvgAttrEditor.tsx
+++ src/components/SvgAttrEditor/SvgAttrEditor.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, SVGProps, useEffect, useState} from "react";
+import React, {FunctionComponent, SVGProps, useEffect, useRef, useState} from "react";
 import './SvgAttrEditor.css'
 
 interface SvgAttrEditorProps {
@@ -9,26 +9,12 @@ export const SvgAttrEditor = ({}:SvgAttrEditorProps) => {
     const [selectedElement, setSelectedElement] = useState<SVGElement | null>(null);
     const [attributes, setAttributes] = useState<Record<string, string>>({});
     const [xmlData, setXmlData] = useState<Document | null>(null);
-    console.log('xmlData', xmlData)
+    const svgContainerRef = useRef<SVGSVGElement | null>(null);
 
     const handleElementClick = (event: React.MouseEvent<SVGElement>) => {
         event.stopPropagation();
         const target = event.target as SVGElement & any;
 
-        console.log('target', target)
-        const parentElement = target.parentNode
-        if(parentElement){
-            console.log('parentElement', parentElement)
-            const deepParentElement = parentElement.parentElement;
-            console.log('deepParentElement', deepParentElement)
-            // console.log(
-            //     "Parent Attributes:",
-            //     Array.from(parentElement.attributes).map((attr: any) => ({
-            //         name: attr.name,
-            //         value: attr.value,
-            //     }))
-            // );
-        }
         if(target.attributes['sbt:seat'] || target.attributes['sbt:cat'] || target.attributes['sbt:row']){
             setSelectedElement(target);
 
@@ -42,6 +28,36 @@ export const SvgAttrEditor = ({}:SvgAttrEditorProps) => {
     };
 
     useEffect(() => {
+        const svgElement = svgContainerRef.current;
+        const handleGroupClick = (event: Event) => {
+            console.log("Группа была нажата", event.currentTarget);
+        };
+        if (svgElement) {
+            const groups = svgElement.querySelectorAll("g");
+            console.log('groups', groups[99]?.attributes)
+            groups.forEach((group) => {
+                // @ts-ignore
+                if(group.attributes['sbt:row'] || group?.attributes['sbt:sect']){
+                    group.addEventListener("click", handleGroupClick);
+                }
+            });
+        }
+
+        // Очистка после рендеринга компонента
+        return () => {
+            if (svgElement) {
+                const groups = svgElement.querySelectorAll("g");
+                groups.forEach((group) => {
+                    // @ts-ignore
+                    if(group.attributes['sbt:row'] || group?.attributes['sbt:sect']){
+                        group.removeEventListener("click", handleGroupClick);
+                    }
+                });
+            }
+        };
+    }, [xmlData]);
+
+    useEffect(() => {
         const fetchXml = async () => {
             // Путь будет работать, если файл находится в public
             const response = await fetch('/exemple.svg');
@@ -50,7 +66,7 @@ export const SvgAttrEditor = ({}:SvgAttrEditorProps) => {
             const xml = parser.parseFromString(text, 'application/xml');
             setXmlData(xml);
         };
-        fetchXml();
+        fetchXml().catch(err => console.log(err));
     }, []);
 
     // Обновление атрибутов
@@ -63,7 +79,7 @@ export const SvgAttrEditor = ({}:SvgAttrEditorProps) => {
 
     return (
         <div style={{ width: '100%', display: "flex", gap: "20px" }}>
-            <svg width="100%" height="100%" viewBox="0 0 500 500">
+            <svg  ref={svgContainerRef} width="100%" height="100%" viewBox="0 0 500 500">
                 <foreignObject x="0" y="0" width="500" height="500" onClick={handleElementClick}>
                     {xmlData &&
                         <div
